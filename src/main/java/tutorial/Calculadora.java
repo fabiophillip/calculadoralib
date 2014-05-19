437928182d7dc60f5b21305665e09ac2503d0fba
fabiophillip <haru_glory6@hotmail.com>
2014-05-19 20:23:55 -0300

    Commit inicial
    commit inicial do projeto calculadora com testes de operações básicas de
    calculadora #1

---
+++ src/main/java/tutorial/Calculadora.java
@@ -0,0 +1,25 @@
+package tutorial;
+
+public class Calculadora {
+	
+	public int somar(int x, int y)
+	{
+		return x + y;
+	}
+	
+	public int subtrair(int x, int y)
+	{
+		return x - y;
+	}
+	
+	public int dividir(int x, int y)
+	{
+		return x / y;
+	}
+	
+	public int multiplicar(int x, int y)
+	{
+		return x * y;
+	}
+
+}
---
+++ src/test/java/tutorial/TesteCalculadora.java
@@ -0,0 +1,36 @@
+package tutorial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TesteCalculadora {
+
+	Calculadora calc;
+	@Before
+	public void setUp() throws Exception {
+		calc = new Calculadora();
+	}
+
+	@Test
+	public void testSoma() {
+		assertEquals(calc.somar(2, 2), 4);
+	}
+	
+	@Test
+	public void testDividir() {
+		assertEquals(calc.dividir(2, 2), 1);
+	}
+	
+	@Test
+	public void testMultiplicar() {
+		assertEquals(calc.dividir(2, 2), 4);
+	}
+	
+	@Test
+	public void testSubtrair() {
+		assertEquals(calc.dividir(2, 2), 0);
+	}
+
+}
