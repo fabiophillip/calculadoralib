52aef1b7a0d5c3c98eac40824dba3ce5ce0586c6
fabiophillip <haru_glory6@hotmail.com>
2014-05-19 21:47:51 -0300

    atualizei testes calculadora para ter log
    usei log4j para gerar log para testes #1

--- src/test/java/tutorial/TesteCalculadora.java
+++ src/test/java/tutorial/TesteCalculadora.java
@@ -2,11 +2,16 @@ package tutorial;
 
 import static org.junit.Assert.*;
 
+import java.util.logging.Level;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.apache.log4j.Logger;
+
 
 public class TesteCalculadora {
 
+	static final Logger logger = Logger.getLogger(TesteCalculadora.class);
 	Calculadora calc;
 	@Before
 	public void setUp() throws Exception {
@@ -15,21 +20,30 @@ public class TesteCalculadora {
 
 	@Test
 	public void testSoma() {
-		assertEquals(calc.somar(2, 2), 4);
+		int resultadoSoma2Mais2 = calc.somar(2, 2);
+		logger.info("resultado testSoma() 2 + 2:" + resultadoSoma2Mais2);
+		assertEquals(resultadoSoma2Mais2, 4);
+		
 	}
 	
 	@Test
 	public void testDividir() {
-		assertEquals(calc.dividir(2, 2), 1);
+		int resultadoDivisao = calc.dividir(2, 2);
+		logger.info("resultado testDividir() 2 / 2:" + resultadoDivisao);
+		assertEquals(resultadoDivisao, 1);
 	}
 	
 	@Test
 	public void testMultiplicar() {
-		assertEquals(calc.multiplicar(2, 2), 4);
+		int resultadoMultiplicacao = calc.dividir(2, 2);
+		logger.info("resultado testMultiplicar() 2 * 2:" + resultadoMultiplicacao);
+		assertEquals(resultadoMultiplicacao, 4);
 	}
 	
 	@Test
 	public void testSubtrair() {
+		int resultadoSubtrair = calc.subtrair(2, 2);
+		logger.info("resultado testSubtrair() 2 - 2:" + resultadoSubtrair);
 		assertEquals(calc.subtrair(2, 2), 0);
 	}
 
