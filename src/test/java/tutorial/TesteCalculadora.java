cd219046171693df8e25b8083b6505ce8af14012
fabiophillip <haru_glory6@hotmail.com>
2014-05-19 22:36:22 -0300

    log4j adicionei properties para log
    log4j adicionei properties para log #1

--- src/test/java/tutorial/TesteCalculadora.java
+++ src/test/java/tutorial/TesteCalculadora.java
@@ -3,10 +3,11 @@ package tutorial;
 import static org.junit.Assert.*;
 
 import java.util.logging.Level;
-
+import tutorial.Calculadora;
 import org.junit.Before;
 import org.junit.Test;
 import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
 
 
 public class TesteCalculadora {
@@ -21,6 +22,8 @@ public class TesteCalculadora {
 	@Test
 	public void testSoma() {
 		int resultadoSoma2Mais2 = calc.somar(2, 2);
+		DOMConfigurator.configure("log4j.xml");
+
 		logger.info("resultado testSoma() 2 + 2:" + resultadoSoma2Mais2);
 		assertEquals(resultadoSoma2Mais2, 4);
 		
@@ -29,6 +32,7 @@ public class TesteCalculadora {
 	@Test
 	public void testDividir() {
 		int resultadoDivisao = calc.dividir(2, 2);
+		DOMConfigurator.configure("log4j.xml");
 		logger.info("resultado testDividir() 2 / 2:" + resultadoDivisao);
 		assertEquals(resultadoDivisao, 1);
 	}
@@ -36,6 +40,7 @@ public class TesteCalculadora {
 	@Test
 	public void testMultiplicar() {
 		int resultadoMultiplicacao = calc.multiplicar(2, 2);
+		DOMConfigurator.configure("log4j.xml");
 		logger.info("resultado testMultiplicar() 2 * 2:" + resultadoMultiplicacao);
 		assertEquals(resultadoMultiplicacao, 4);
 	}
@@ -43,6 +48,7 @@ public class TesteCalculadora {
 	@Test
 	public void testSubtrair() {
 		int resultadoSubtrair = calc.subtrair(2, 2);
+		DOMConfigurator.configure("log4j.xml");
 		logger.info("resultado testSubtrair() 2 - 2:" + resultadoSubtrair);
 		assertEquals(calc.subtrair(2, 2), 0);
 	}
